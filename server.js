13d96fd9aa49223cc2bc26f743164c3297ec71d1
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-09-27 02:14:36 -0500

    added get and post routes

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const { notes } = require('./db/db')
 
 const express = require('express');
-const { fstat } = require('fs');
+const fs  = require('fs');
 
 const app = express();
 
-app.get('/api/')
+app.get('/api/notes', (req, res) => {
+    res.json('db');
+});
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    res.json(result);
+});
+
+api.post('/api/animals', (res, req) => {
+
+});
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
